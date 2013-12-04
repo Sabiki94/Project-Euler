b8abbf21d6c10ba5975aaaeee8a57b98386c28e7
Sabiki94 <jptomoya@gmail.com>
2013-12-05 00:54:36 +0900

    Problem 12: Named variable more easy to understand. Improve algorithm.

--- Problem_0012/Problem_12.c
+++ Problem_0012/Problem_12.c
@@ -3,23 +3,22 @@
 
 #include <stdio.h>
 
-#define GOAL 501
+#define GOAL 500
 
 int main(void){
-  int i, t_num = 0, count = 0, tt = 0, maxtemp = 0;
+  int i, t_num = 0, divisor_count = 0, tt = 0, maxtemp = 0;
 
-  while(1){
-    tt++; // Triangle num temp
-    t_num += tt;
+  while (divisor_count < GOAL){
+      tt++; // Triangle num temp
+      t_num += tt;
 
-    count = 0;
-    maxtemp = t_num;
-    for(i = 1; i < maxtemp; i++)
-      if(t_num % i == 0){
-	count += 2;
-	maxtemp = t_num / i;
+      divisor_count = 0;
+      maxtemp = t_num;
+      for (i = 1; i < maxtemp; i++)
+      if (t_num % i == 0){
+          divisor_count += 2;
+          maxtemp = t_num / i;
       }
-    if(count >= GOAL) break;
   }
 
   printf("t_num : %d\n", t_num);
