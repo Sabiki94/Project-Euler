87980c93c2773d7a010452d477e9fdf0943bfeda
Sabiki94 <jptomoya@gmail.com>
2013-12-05 00:39:52 +0900

    Use stdint.h instead of windows.h to improve portability. Inprove progress message.

--- Problem_0010/Problem_10.c
+++ Problem_0010/Problem_10.c
@@ -1,16 +1,17 @@
 /* Problem_10.c */
 /* 2012-07-13 */
 /* 2012-11-07 */
+/* 2013-12-05 */
 
 #include <stdio.h>
-#include <Windows.h>
+#include <stdint.h>
 #include <math.h>
 
 #define NUM 2000000
 
-UINT64 nextpnum(UINT64 num)
+uint64_t nextpnum(uint64_t num)
 {
-  UINT64 count = 2;
+  uint64_t count = 2;
 
   if(num % 2)	num+=2;
   else num++;
@@ -28,13 +29,16 @@ UINT64 nextpnum(UINT64 num)
 
 int main(void)
 {
-  UINT64 sum = 0, ptemp = 0;
+  uint64_t sum = 0, ptemp = 0;
 
-  while(ptemp <= NUM){
-    sum += ptemp;
-    ptemp = nextpnum(ptemp);
-	printf("%.2f%%\n", (ptemp * 100.0) / NUM);
+  printf("Calculating...\n");
+  while (ptemp <= NUM){
+      putchar('\r');
+      sum += ptemp;
+      ptemp = nextpnum(ptemp);
+      printf("%.2f%%\r", (ptemp * 100.0) / NUM);
   }
+  printf("done.  \n");
 
   printf("The sum of all the primes below 2000000 : %llu\n", sum);
 
