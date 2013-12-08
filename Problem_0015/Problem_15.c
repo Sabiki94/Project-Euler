90afe5aea7131a8fd02d9f3b776fb93637918e6d
Sabiki94 <jptomoya@gmail.com>
2013-12-08 20:43:49 +0900

    Implement Problem 15.

--- Problem_0015/Problem_15.c
+++ Problem_0015/Problem_15.c
@@ -1,22 +1,46 @@
 /* Problem_15.c */
 /* 2012-07-14 */
-/* UNDONE */
+/* 2013-12-08 */
+
+/* Calculate 40 C 20 */
+/* Using below mathematical property :  */
+/* (nC0)^2 + (nC1)^2 + ... + (nCn)^2 == 2nCn  */
 
 #include <stdio.h>
+#include <stdint.h>
+
+#define N 40
+#define M 20
+
+int combination(int n, int m)
+{
+    int i;
+    uint64_t nur, deno;
+
+    nur = 1;
+    for (i = n; i >= n-m+1; i--) {
+        nur *= i;
+    }
+
+    deno = 1;
+    for (i = 1; i <= m; i++) {
+        deno *= i;
+    }
+
+    return (int)(nur/deno);
+}
 
 int main(void)
 {
-  /* calcrate 40C20 */
-  unsigned long long int i, nur = 0, deno = 0, temp = 20;
+    int i;
+    uint64_t result;
 
-  for(i = 40; i >= 21; i--){
-    nur *= i;
-  }
-  for(i = temp; i >= 1; i--)
-    deno *= i;
+    result = 0;
+    for (i = 0; i <= M; i++) {
+        result += (uint64_t)combination(M, i) * (uint64_t)combination(M, i);
+    }
 
-  printf("%llu / %llu\n", nur, deno);
-  printf("number of routes : %llu\n", nur / deno);
+  printf("number of routes : %llu\n", result);
 
   return 0;
 }
